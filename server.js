d802db8bcf6e4478862442e8e55566becabd59c2
SlavicRaven <krzysztof.rabenda93@gmail.com>
2016-12-22 11:13:49 +0100

    Commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html')
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(index.html);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(index);
 }).listen(process.env.PORT);
