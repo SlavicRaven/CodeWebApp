4a8510ecf4ccfc735cd25141ec2c8fef28fdde1c
SlavicRaven <krzysztof.rabenda93@gmail.com>
2016-12-22 10:47:17 +0100

    Commit 5

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Dlaczego to nie dziala</h1>');
+    res.end('<h1>Raz Raz</h1> <h2> Dwa Dwa</h2>');
 }).listen(process.env.PORT);
