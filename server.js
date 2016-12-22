bbf32483b749639910439d4631ddc8501188da67
SlavicRaven <krzysztof.rabenda93@gmail.com>
2016-12-22 11:51:50 +0100

    commit

--- server.js
+++ server.js
@@ -1,32 +1,40 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html')
-var pozycjeH5 = fs.readFileSync('pozycjeH5.css')
+var http = require('http'),
+    fs = require('fs');
 
-var server = http.createServer(function (request, response) {
-    fs.readFile('./' + request.url, function(err, data) {
-        if (!err) {
-            var dotoffset = request.url.lastIndexOf('.');
-            var mimetype = dotoffset == -1
-                            ? 'text/plain'
-                            : {
-                                '.html' : 'text/html',
-                                '.ico' : 'image/x-icon',
-                                '.jpg' : 'image/jpeg',
-                                '.png' : 'image/png',
-                                '.gif' : 'image/gif',
-                                '.css' : 'text/css',
-                                '.js' : 'text/javascript'
-                                }[ request.url.substr(dotoffset) ];
-            response.setHeader('Content-type' , mimetype);
-            response.end(data);
-            console.log( request.url, mimetype );
-        } else {
-            console.log ('file not found: ' + request.url);
-            response.writeHead(404, "Not Found");
-            response.end();
-        }
-    });
-}
+http.createServer(function (req, res) {
 
-.listen(process.env.PORT)};
+    if(req.url.indexOf('.html') != -1){ //req.url has the pathname, check if it conatins '.html'
+
+      fs.readFile(__dirname + '/public/chatclient.html', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+
+    if(req.url.indexOf('.js') != -1){ //req.url has the pathname, check if it conatins '.js'
+
+      fs.readFile(__dirname + '/public/js/script.js', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+
+    if(req.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
+
+      fs.readFile(__dirname + '/public/css/style.css', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
