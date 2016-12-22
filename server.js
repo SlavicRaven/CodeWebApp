cbfeff48348166053ea646ab62c742629a392a86
SlavicRaven <krzysztof.rabenda93@gmail.com>
2016-12-22 11:23:54 +0100

    Commit

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var http = require('http');
 var fs = require('fs');
 var index = fs.readFileSync('index.html')
+var pozycjeH5 = fs.readFileSync('pozycjeH5.css')
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
+    // GET HTML
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
     res.end(index);
+    // GET CSS
+    res.writeHead(200, {'Content-Type': 'text/css; charset=utf-8'});
+    res.end(pozycjeH5);
 }).listen(process.env.PORT);
