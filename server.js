e2c909ce0584be79009ef307a2cb234426adf880
SlavicRaven <krzysztof.rabenda93@gmail.com>
2016-12-22 12:11:56 +0100

    commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
 
     if(req.url.indexOf('.html') != -1){ //req.url has the pathname, check if it conatins '.html'
 
-      fs.readFile(__dirname + '/public/chatclient.html', function (err, data) {
+      fs.readFile(__dirname + 'index.html', function (err, data) {
         if (err) console.log(err);
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
@@ -14,20 +14,9 @@ http.createServer(function (req, res) {
 
     }
 
-    if(req.url.indexOf('.js') != -1){ //req.url has the pathname, check if it conatins '.js'
-
-      fs.readFile(__dirname + '/public/js/script.js', function (err, data) {
-        if (err) console.log(err);
-        res.writeHead(200, {'Content-Type': 'text/javascript'});
-        res.write(data);
-        res.end();
-      });
-
-    }
-
     if(req.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
 
-      fs.readFile(__dirname + '/public/css/style.css', function (err, data) {
+      fs.readFile(__dirname + 'style.css', function (err, data) {
         if (err) console.log(err);
         res.writeHead(200, {'Content-Type': 'text/css'});
         res.write(data);
